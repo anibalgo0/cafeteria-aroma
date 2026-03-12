1b6b8993c358bb802bbdaaba171f219605992e3d
Anibal XVII <tortuganija123@gmail.com>
2026-03-11 23:00:12 -0500

    Update script.js with reservation functionality

--- script.js
+++ script.js
@@ -2,71 +2,47 @@ function reservar(){
 alert("Mesa reservada ☕\nTe esperamos en Cafetería Aroma");
 }
 
+/* ocultar todas */
+
 function ocultarTodo(){
+
 document.getElementById("inicio").style.display="none";
 document.getElementById("galeria").style.display="none";
 document.getElementById("evento").style.display="none";
 document.getElementById("contacto").style.display="none";
-}
-
-/* controlar boton */
-function controlarBoton(seccion){
-
-let boton = document.querySelector(".whatsapp-float");
-
-if(seccion === "inicio" || seccion === "contacto"){
-boton.style.display = "none";
-}else{
-boton.style.display = "flex";
-}
 
 }
 
-/* cuando carga la pagina */
+/* mostrar seccion */
+
+function mostrarSeccion(seccion){
+
+ocultarTodo();
+
+document.getElementById(seccion).style.display="block";
+
+}
+
+/* detectar cambio de URL */
+
+window.addEventListener("hashchange", function(){
+
+let seccion = location.hash.replace("#","");
+
+if(seccion === "") seccion = "inicio";
+
+mostrarSeccion(seccion);
+
+});
+
+/* cuando carga la página */
+
 window.onload = function(){
-controlarBoton("inicio");
-}
 
-/* inicio */
-document.querySelector('a[href="#inicio"]').addEventListener("click", function(){
+let seccion = location.hash.replace("#","");
 
-ocultarTodo();
-document.getElementById("inicio").style.display="block";
-controlarBoton("inicio");
+if(seccion === "") seccion = "inicio";
 
-});
+mostrarSeccion(seccion);
 
-function mostrarEvento(){
-
-ocultarTodo();
-document.getElementById("evento").style.display="block";
-controlarBoton("evento");
-
-}
-
-/* galeria */
-document.querySelector('a[href="#galeria"]').addEventListener("click", function(){
-
-ocultarTodo();
-document.getElementById("galeria").style.display="block";
-controlarBoton("galeria");
-
-});
-
-/* evento */
-document.querySelector('a[href="#evento"]').addEventListener("click", function(){
-
-ocultarTodo();
-document.getElementById("evento").style.display="block";
-controlarBoton("evento");
-
-});
-
-/* contacto */
-document.querySelector('a[href="#contacto"]').addEventListener("click", function(){
-
-ocultarTodo();
-document.getElementById("contacto").style.display="block";
-controlarBoton("contacto");
-
-});
+};
